0f6f5ffbb476f10c64fe946aa63da38cb1d2e23c
Pukhova Olga <pukhova.olg@gmail.com>
2013-04-08 23:48:55 +0300

    initial commit for android app

---
+++ src/com/zeppelin/mygame/MainActivity.java
@@ -0,0 +1,43 @@
+package com.zeppelin.mygame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	protected Button Start;
+	protected Button Settings;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Connect interface elements to properties
+		Start = (Button)findViewById(R.id.game_start);
+		Settings = (Button)findViewById(R.id.game_settings);
+
+		// Setup ClickListeners
+		Start.setOnClickListener(this);
+		Settings.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v == Start){}
+		else if (v == Settings){
+
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
